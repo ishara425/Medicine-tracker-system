c6703d0b14243794ffe34a11ec7a670baddcb067
Ishara Palangasinghe <145181123+ishara425@users.noreply.github.com>
2025-08-10 13:01:14 +0530

    update

---
+++ backend/Medicine-tracker/src/main/java/com/med/Medicine_tracker/MedicineTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.med.Medicine_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicineTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicineTrackerApplication.class, args);
+	}
+
+}
